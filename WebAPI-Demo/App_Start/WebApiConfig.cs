f60a8f8acd52e31d6fb8ab93e7e1a2efbff0b220
Komal NalawKomalade <komal_nalawade@outlook.com>
2021-11-28 11:43:57 +0530

    Create Custom JSON Media Formatter

--- WebAPI-Demo/App_Start/WebApiConfig.cs
+++ WebAPI-Demo/App_Start/WebApiConfig.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebAPI_Demo
 {
     public static class WebApiConfig
     {
+        //Approach 2
+        public class CustomJsonFormatter : JsonMediaTypeFormatter
+        {
+            public CustomJsonFormatter()
+            {
+                this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            }
+            public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+            {
+                base.SetDefaultContentHeaders(type, headers, mediaType);
+                headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+        }
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
@@ -19,10 +34,22 @@ namespace WebAPI_Demo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            //Approach 2
+            config.Formatters.Add(new CustomJsonFormatter());
+            //Approach 1
+            //By default our web api return XML
+            // How to return JSON Instead of XML
+            //config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            //How to return only XML
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);
 
-            //output in json and properties name in camel case
-            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
-            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+            //How to return only JSON
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //output in json and properties name in camel case 
+            //Content Negotiation
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
 
         }
     }
