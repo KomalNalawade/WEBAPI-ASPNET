f6c7295899d99bb2c8c69bdd00a1c68949a418dd
Komal NalawKomalade <komal_nalawade@outlook.com>
2021-11-28 08:38:30 +0530

    Content Negotiation

--- WebAPI-Demo/App_Start/WebApiConfig.cs
+++ WebAPI-Demo/App_Start/WebApiConfig.cs
@@ -19,6 +19,11 @@ namespace WebAPI_Demo
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            //output in json and properties name in camel case
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
         }
     }
 }
